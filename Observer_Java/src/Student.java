c436b94faa1c94bd4db5d3d571dd39aba1427e23
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-06-08 21:20:48 +0530

    update student

--- Observer_Java/src/Student.java
+++ Observer_Java/src/Student.java
@@ -1,6 +1,13 @@
 public class Student implements StudentObserver{
+
+    String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
     @Override
     public void update(String message) {
-    
+        System.out.println(name + " Got the Message " + message);
     }
 }
